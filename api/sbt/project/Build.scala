ec40330596ecf13b0e4967e01569ac19d700a3ef
Yu Changyuan <reivzy@gmail.com>
2012-11-04 18:54:08 +0800

    Add sbt project files for generate apklib.
    The project depends on version 0.6.3-SNAPSHOT of sbt-android-plugin to
    generate apklib.

---
+++ api/sbt/project/Build.scala
@@ -0,0 +1,36 @@
+import sbt._
+
+import Keys._
+import AndroidKeys._
+
+object General {
+  val settings = Defaults.defaultSettings ++ Seq (
+    name := "astrid-api",
+    organization := "com.todoroo",
+    version := "1.0",
+    versionCode := 1,
+    scalaVersion := "2.9.2",
+    crossPaths := false,
+    platformName in Android := "android-14"
+  )
+
+  val proguardSettings = Seq (
+    useProguard in Android := false
+  )
+
+  lazy val fullAndroidSettings =
+    General.settings ++
+    AndroidProject.androidSettings ++
+    proguardSettings ++
+    AndroidManifestGenerator.settings ++
+    addArtifact(Artifact("astrid-api", "apklib", "apklib"),
+                apklibPackage in Android).settings
+}
+
+object AndroidBuild extends Build {
+  lazy val main = Project (
+    "astrid-api",
+    file("."),
+    settings = General.fullAndroidSettings
+  )
+}
